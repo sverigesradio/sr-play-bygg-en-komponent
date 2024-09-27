3dada388baa8ebe679e2f4deefd2b6adea4478ef
natlus <jesper.sultan@gmail.com>
2024-09-27 09:07:48 +0200

    use new data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,89 +1,38 @@
-"use client";
-
-export default function Home() {
+export default async function Home() {
   return (
-    <div className="grid grid-flow-col gap-5 p-5">
-      <div />
+    <div className="grid grid-cols-3 gap-2 p-2">
+      <div>...content</div>
     </div>
   );
 }
 
 const mock = {
-  id: 2339459,
-  title: "Bret Easton Ellis – i huvudet på ”Mr American psycho”",
+  id: 2447659,
+  title: "Muhammad Ali – mästaren och motståndet",
   description:
-    "Bret Easton Ellis anser sig ha blivit svartlistad av både litteraturvärlden och av Hollywood –&nbsp;med 30 år emellan. P3 ID möter kultförfattaren som skildrar sex, knark och seriemördare.",
-  publisher: "Caroline Pouron",
-  publishDate: "2024-02-12T04:45:00Z",
-  links: {
-    self: "https://open-api.sr.se/v3/episodes/2339459",
-    html: "https://sverigesradio.se/avsnitt/bret-easton-ellis",
-  },
+    "Han är proffsboxaren som blev en obeveklig symbol för motstånd. Med sin charm och sina snabba fötter slog han knockout på en hel värld, men hans värsta fight skulle inte bli i ringen.",
   program: {
     id: 5343,
     name: "P3 ID",
-    links: { self: "https://open-api.sr.se/v3/programs/5343" },
   },
-  branding: { name: "P3", color: "#19a972" },
-  language: { code: "sv", direction: "ltr" },
-  image: {
-    title: "Bret Easton Ellis gestikulerar med händerna",
-    photographer: "Heiko Junge/TT/SR",
-    variants: [
-      {
-        url: "https://static-cdn.sr.se/images/5343/9b98b124-8140-44c1-8aab-6cd88ca93b6d.jpg?preset=2048x1152",
-        width: 2048,
-        height: 1152,
-      },
-      {
-        url: "https://static-cdn.sr.se/images/5343/9b98b124-8140-44c1-8aab-6cd88ca93b6d.jpg?preset=1024x576",
-        width: 1024,
-        height: 576,
-      },
-      {
-        url: "https://static-cdn.sr.se/images/5343/9b98b124-8140-44c1-8aab-6cd88ca93b6d.jpg?preset=512x288",
-        width: 512,
-        height: 288,
-      },
-      {
-        url: "https://static-cdn.sr.se/images/5343/9b98b124-8140-44c1-8aab-6cd88ca93b6d.jpg?preset=256x144",
-        width: 256,
-        height: 144,
-      },
-    ],
+  audiopreference: "pod",
+  audiopriority: "mp3",
+  audiopresentation: "format",
+  imageurl:
+    "https://static-cdn.sr.se/images/5343/2e99637b-9cb4-4f33-9cba-c04d0f208d1f.jpg?preset=api-default-square",
+  listenpodfile: {
+    title: "Muhammad Ali – mästaren och motståndet",
+    description: "",
+    filesizeinbytes: 66527209,
+    program: {
+      id: 5343,
+      name: "P3 ID",
+    },
+    availablefromutc: "/Date(1724643900000)/",
+    duration: 4157,
+    publishdateutc: "/Date(1724643900000)/",
+    id: 9452911,
+    url: "https://sverigesradio.se/topsy/ljudfil/srapi/9452911.mp3",
+    statkey: "/app/avsnitt/p3[k(164)]/p3 id[p(5343)]/[e(2447659)]",
   },
-  audios: [
-    {
-      duration: 4607,
-      type: "pod",
-      variants: [
-        {
-          type: "mp3",
-          url: "https://sverigesradio.se/topsy/ljudfil/srapi/9203599.mp3",
-          bitrate: 128000,
-        },
-      ],
-    },
-    {
-      duration: 4607,
-      type: "broadcast",
-      variants: [
-        {
-          type: "aac",
-          url: "https://sverigesradio.se/topsy/ljudfil/srapi/9203595-hi.html5desktop",
-          bitrate: 192000,
-        },
-        {
-          type: "aac",
-          url: "https://sverigesradio.se/topsy/ljudfil/srapi/9203595.html5desktop",
-          bitrate: 96000,
-        },
-        {
-          type: "aac",
-          url: "https://sverigesradio.se/topsy/ljudfil/srapi/9203595-lo.html5desktop",
-          bitrate: 32000,
-        },
-      ],
-    },
-  ],
 };
